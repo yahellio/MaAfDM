c8dfb020448c887074142b9f9e31fd778cb43e82
yahellio <yayakn1h@gmail.com>
2025-04-07 02:12:57 +0300

    Lab6

---
+++ Lab6/MIAPR_6/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace MIAPR_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioBtnMaximum = new System.Windows.Forms.RadioButton();
+            this.radioBtnMinimum = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.ColumnHeadersVisible = false;
+            this.dataGridView.Location = new System.Drawing.Point(801, 233);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.Size = new System.Drawing.Size(251, 162);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(145, 564);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(463, 78);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Классификация объектов";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(31, 24);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(710, 504);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(797, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 23);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Количество объектов:";
+            // 
+            // radioBtnMaximum
+            // 
+            this.radioBtnMaximum.AutoSize = true;
+            this.radioBtnMaximum.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioBtnMaximum.Location = new System.Drawing.Point(905, 64);
+            this.radioBtnMaximum.Name = "radioBtnMaximum";
+            this.radioBtnMaximum.Size = new System.Drawing.Size(72, 27);
+            this.radioBtnMaximum.TabIndex = 1;
+            this.radioBtnMaximum.TabStop = true;
+            this.radioBtnMaximum.Text = "Max";
+            this.radioBtnMaximum.UseVisualStyleBackColor = true;
+            // 
+            // radioBtnMinimum
+            // 
+            this.radioBtnMinimum.AutoSize = true;
+            this.radioBtnMinimum.Checked = true;
+            this.radioBtnMinimum.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioBtnMinimum.Location = new System.Drawing.Point(802, 64);
+            this.radioBtnMinimum.Name = "radioBtnMinimum";
+            this.radioBtnMinimum.Size = new System.Drawing.Size(67, 27);
+            this.radioBtnMinimum.TabIndex = 0;
+            this.radioBtnMinimum.TabStop = true;
+            this.radioBtnMinimum.Text = "Min";
+            this.radioBtnMinimum.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(797, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(306, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Критерий классификации:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(802, 173);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(71, 31);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "5";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1142, 662);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioBtnMaximum);
+            this.Controls.Add(this.radioBtnMinimum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView);
+            this.Font = new System.Drawing.Font("Times New Roman", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ЛАБОРАТОРНАЯ РАБОТА 6";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioBtnMaximum;
+        private System.Windows.Forms.RadioButton radioBtnMinimum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Lab6/MIAPR_6/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MIAPR_6
+{
+    public partial class Form1 : Form
+    {
+        private double[,] distances;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ApplyModernUI();
+        }
+
+        private void ApplyModernUI()
+        {
+            // Применяем шрифт и цветовую схему
+            this.Font = new Font("Segoe UI", 10);
+            this.BackColor = Color.FromArgb(34, 34, 34);  // Темный фон
+            this.ForeColor = Color.White; // Белый текст
+
+            // Устанавливаем стиль кнопок с закругленными углами
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.FlatAppearance.BorderColor = Color.FromArgb(0, 123, 255); // Синий цвет для рамки
+            button1.BackColor = Color.FromArgb(52, 58, 64); // Темный цвет кнопки
+            button1.ForeColor = Color.White; // Белый текст на кнопке
+            button1.Font = new Font("Segoe UI", 12, FontStyle.Bold); // Шрифт для кнопки
+
+            // Изменяем стиль TextBox
+            textBox1.BackColor = Color.FromArgb(52, 58, 64);  // Тёмный фон для текстового поля
+            textBox1.ForeColor = Color.White;  // Белый текст
+            textBox1.Font = new Font("Segoe UI", 12);
+
+            // Изменяем стиль DataGridView
+            dataGridView.BackgroundColor = Color.FromArgb(40, 44, 52); // Темный фон
+            dataGridView.DefaultCellStyle.BackColor = Color.FromArgb(52, 58, 64); // Темный фон ячеек
+            dataGridView.DefaultCellStyle.ForeColor = Color.White; // Белый текст в ячейках
+            dataGridView.DefaultCellStyle.SelectionBackColor = Color.FromArgb(0, 123, 255); // Синий фон для выделенной ячейки
+
+            // Устанавливаем стиль для легенды на графике
+            foreach (var legend in chart1.Legends)
+            {
+                legend.BackColor = Color.FromArgb(52, 58, 64);  // Темный фон для легенды
+                legend.ForeColor = Color.White; // Белый текст
+                legend.Font = new Font("Segoe UI", 10, FontStyle.Bold); // Шрифт
+            }
+
+            // Настройка графика
+            chart1.BackColor = Color.Black;
+            chart1.ChartAreas[0].BackColor = Color.Black;
+            chart1.ChartAreas[0].AxisX.LineColor = Color.White;
+            chart1.ChartAreas[0].AxisY.LineColor = Color.White;
+            chart1.ChartAreas[0].AxisX.LabelStyle.ForeColor = Color.White;
+            chart1.ChartAreas[0].AxisY.LabelStyle.ForeColor = Color.White;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < '0' || e.KeyChar > '9') && e.KeyChar != 8 && e.KeyChar != '-')
+                e.KeyChar = '\0';
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            distances = SetRandomGrid(int.Parse(textBox1.Text));
+
+            var hierarchical = new HierarchicalGrouping(distances, int.Parse(textBox1.Text));
+
+            hierarchical.FindGroups();
+
+            // Настройка цвета линий на графике
+            foreach (var series in chart1.Series)
+            {
+                series.BorderColor = Color.Cyan;
+                series.BorderWidth = 2;
+                series.MarkerSize = 0;
+                series.IsValueShownAsLabel = true;
+                series.LabelForeColor = Color.White;
+            }
+
+            // Рисуем график с анимацией
+            hierarchical.Draw(chart1);
+        }
+
+        private double[,] SetRandomGrid(int size)
+        {
+            dataGridView.ColumnCount = size + 1;
+            dataGridView.RowCount = size + 1;
+
+            for (int i = 0; i < size; i++)
+            {
+                dataGridView[0, i + 1].Value = string.Format("x{0}", i + 1);
+                dataGridView[i + 1, 0].Value = string.Format("x{0}", i + 1);
+            }
+
+            var result = new double[size, size];
+            var rand = new Random();
+
+            for (int i = 0; i < size; i++)
+                result[i, i] = 0;
+            for (int i = 1; i < size; i++)
+                for (int j = 0; j < i; j++)
+                {
+                    result[i, j] = rand.Next(size + 5) + 1;
+                    result[j, i] = result[i, j];
+                }
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    dataGridView[i + 1, j + 1].Value = result[i, j];
+            if (radioBtnMaximum.Checked)
+            {
+                for (int i = 1; i < int.Parse(textBox1.Text); i++)
+                    for (int j = 0; j < i; j++)
+                    {
+                        result[i, j] = size + 6 - result[i, j];
+                        result[j, i] = result[i, j];
+                    }
+            }
+
+            return result;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Дополнительные действия при загрузке формы, если необходимо
+        }
+    }
+}
